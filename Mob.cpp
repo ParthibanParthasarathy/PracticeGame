b3308e96257d7fbee67bbd518903399f4ba2e6e0
ParthibanParthasarathy <parthi.partha@outlook.com>
2025-04-26 06:25:00 -0500

    Turns out .gitignore wasn't in utf-8 encoding :( Now it should work.

---
+++ Mob.cpp
@@ -0,0 +1,54 @@
+//implementation file
+//constructor prototype that argumenrts can be passed in example: Mob(string, int, int, int )
+//this would be in the mob.h file, implementation would be like Mob::Mob(string n, int a, etc...)
+//would be passed in the main file like Mob enemy("Bob", 15, 16, 78)
+//we are not using this because I'd rather do all of them separatly because I want to have more stats.
+
+#include "Mob.h"
+
+Mob::Mob() {//this is a default constructor. two of these default contsructors will result in a did not compile.
+    name = "John Doe";
+    hp = 15;
+    atk = 15;
+    def = 15;
+
+
+}
+Mob::Mob(std::string n, int h, int a, int d) { //second overloaded constructor. non consrtuctor member functions can also be overloaded.
+    name = n;
+    hp = h;
+    atk = a;
+    def = d;
+}
+
+void Mob::setHP(int health) {
+    hp = health;
+}
+int Mob::getHP() const{
+    return hp;
+}
+
+void Mob::setATK(int attack) {
+    atk = attack;
+}
+int Mob::getATK() const{
+    return atk;
+}
+
+void Mob::setDEF(int defense) {
+    def = defense;
+}
+int Mob::getDEF() const{
+    return def;
+}
+
+void Mob::setName(std::string n) {
+    name = n;
+}
+std::string Mob::getName() const{
+    return name;
+}
+
+Mob::~Mob(){//can only be one destructor cause destructors dont take arguments
+    delete unsafeGoldPouch;
+}
---
+++ main.cpp
@@ -0,0 +1,72 @@
+//no need for include iostream and include string because it is included in the Mob.h
+#include "Mob.h"
+
+
+
+
+int main() {
+
+    int tempInt;
+    std::string hello = "Hello World!";    
+    std::cout << hello << std::endl;
+
+    
+
+    
+
+
+    Mob me;
+    //std::cout<<me.getDEF(); testing if the contructor works. It does in fact work.
+
+    
+    //makes the enemy dynamic, allowing us to delete and make new ones without using up storage.
+    Mob *enemyPtr = nullptr;
+
+    enemyPtr = new Mob;
+
+    enemyPtr->setHP(10);
+    enemyPtr->setATK(10);
+    enemyPtr->setDEF(10);
+    enemyPtr->setName("bob");
+
+    std::cout<<"What hp do you want?"<<std::endl;
+    std::cin>>tempInt;
+    me.setHP(tempInt);
+    me.setATK(2);
+    me.setDEF(10);
+    me.setName("Me");
+
+
+    
+    //derefrencing enemyPtr to get the hp. remeber that the dot operator has precedence, so parenthesis were used. enemyPtr->getHP() is the other way to use it.
+    while((*enemyPtr).getHP() > 0 && me.getHP()>0) {
+        std::cout<<"FIGHT!"<<std::endl;
+        enemyPtr->setHP(enemyPtr->getHP()-me.getATK());
+        me.setHP(me.getHP() - enemyPtr->getATK());
+        std::cout<<"Your HP is "<<me.getHP()<<std::endl;
+        std::cout<<"Enemy HP is "<<enemyPtr->getHP()<<std::endl;
+
+    }
+    delete enemyPtr; //destroys the enemy after to free up space. can also use a destructor
+    enemyPtr = nullptr;
+    if(me.getHP()>0) {
+        std::cout<<"You won!! Good job!!"<<std::endl;
+    }
+    else {
+        std::cout<<"You lost!!!!! HAHAHAHAHAAHAHA!!!!!!"<<std::endl;
+    }
+    
+    while(true) {
+        std::string quit;
+        std::cin>>quit;
+        if (quit == "q") {
+            exit(0);
+        }
+
+    }
+
+
+    
+    
+}
+
